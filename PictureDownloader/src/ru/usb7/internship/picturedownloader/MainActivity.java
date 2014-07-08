fb6ce6850d31f73106e49027f605b21f4f79ee66
Yuriy Nazarov <nazarov@chesapeake>
2014-07-08 20:17:55 +0400

    GUI controls added.

--- PictureDownloader/src/ru/usb7/internship/picturedownloader/MainActivity.java
+++ PictureDownloader/src/ru/usb7/internship/picturedownloader/MainActivity.java
@@ -2,14 +2,12 @@ package ru.usb7.internship.picturedownloader;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
 
 public class MainActivity extends Activity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-    }
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+	}
 }
