58638ad7ea22143ad64d0dc8caf98dd40f154c9e
Yuriy Nazarov <nazarov@chesapeake>
2014-07-10 21:02:07 +0400

    Logging removed, cleanup.

--- PictureDownloader/src/ru/usb7/internship/picturedownloader/MainActivity.java
+++ PictureDownloader/src/ru/usb7/internship/picturedownloader/MainActivity.java
@@ -6,11 +6,11 @@ import ru.usb7.internship.picturedownloader.PicrureLoaderResult.LoaderResult;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
+import android.os.Looper;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.app.LoaderManager;
 import android.support.v4.content.FileProvider;
 import android.support.v4.content.Loader;
-import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
@@ -19,7 +19,6 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends FragmentActivity implements LoaderManager.LoaderCallbacks<PicrureLoaderResult> {
-	private static final String TAG = "MyTag";
 	private static final int LOADER_ID = 1;
 	private static String pictureName = "logo.png";
 	private static OnClickListener openClickListener;
@@ -31,43 +30,29 @@ public class MainActivity extends FragmentActivity implements LoaderManager.Load
 		setContentView(R.layout.activity_main);
 		getSupportLoaderManager().initLoader(LOADER_ID, null, this);
 
-		final MainActivity activity = this;
 		downloadClickListener = new View.OnClickListener() {
-			public void onClick(View v) {
-				Log.d(TAG, "onClick downloadClickListener");
-				getSupportLoaderManager().initLoader(LOADER_ID, null, activity).forceLoad();
-				v.setEnabled(false);
-				ProgressBar pb = (ProgressBar) findViewById(R.id.progressBarDownloading);
-				pb.setVisibility(View.VISIBLE);
+			public void onClick(View button) {
+				getSupportLoaderManager().getLoader(LOADER_ID).forceLoad();
+				button.setEnabled(false);
 			}
 		};
 		openClickListener = new View.OnClickListener() {
 			public void onClick(View v) {
-				Log.d(TAG, "onClick openClickListener");
 				Uri contentUri = FileProvider.getUriForFile(getApplicationContext(),
 						"ru.usb7.internship.picturedownloader.fileprovider", // formatting
-						new File(getApplicationContext().getFilesDir(), pictureName));
+						new File(getFilesDir(), pictureName));
 
 				Intent intent = new Intent();
 				intent.setAction(Intent.ACTION_VIEW);
 				intent.setFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
-				intent.setDataAndType(contentUri, "image/png");
+				intent.setDataAndType(contentUri, "image/*");
 				startActivity(intent);
 			}
 		};
 		setBtnDownload();
 	}
 
-	@Override
-	protected void onDestroy() {
-		Log.d(TAG, "onDestroy");
-		File file = new File(getApplicationContext().getFilesDir(), pictureName);
-		file.delete();
-		super.onDestroy();
-	}
-
 	private void setBtnDownload() {
-		Log.d(TAG, "setBtnDownload");
 		final Button button = (Button) findViewById(R.id.buttonDownloadOpen);
 		button.setText(getResources().getString(R.string.button_download_title));
 		button.setOnClickListener(downloadClickListener);
@@ -75,7 +60,6 @@ public class MainActivity extends FragmentActivity implements LoaderManager.Load
 	}
 
 	private void setBtnOpen() {
-		Log.d(TAG, "setBtnOpen");
 		final Button button = (Button) findViewById(R.id.buttonDownloadOpen);
 		button.setText(getResources().getString(R.string.button_open_title));
 		button.setOnClickListener(openClickListener);
@@ -83,40 +67,38 @@ public class MainActivity extends FragmentActivity implements LoaderManager.Load
 	}
 
 	@Override
-	public void onLoadFinished(Loader<PicrureLoaderResult> loader, PicrureLoaderResult data) {
-		final PicrureLoaderResult fdata = data;
-		runOnUiThread(new Runnable() {
-			public void run() {
-
-				Log.d(TAG, "onLoadFinished");
-				TextView tv = (TextView) findViewById(R.id.textViewStatusValue);
-				ProgressBar pb = (ProgressBar) findViewById(R.id.progressBarDownloading);
-
-				if (fdata.loaderResult == LoaderResult.LOADED) {
-					tv.setText(getResources().getString(R.string.status_loaded));
-					setBtnOpen();
-					pb.setVisibility(View.INVISIBLE);
-					Log.d(TAG, "tv1 " + tv + " th " + android.os.Process.myTid());
-				} else if (fdata.loaderResult == LoaderResult.FAILED) {
-					tv.setText(getResources().getString(R.string.status_idle));
-					Toast.makeText(getApplicationContext(),
-							getResources().getString(R.string.downloading_failed_toast), Toast.LENGTH_SHORT).show();
-					setBtnDownload();
-					pb.setVisibility(View.INVISIBLE);
-					Log.d(TAG, "tv2 " + tv + " th " + android.os.Process.myTid());
-				} else {
-					Log.d(TAG, "tv31 " + tv + " th " + android.os.Process.myTid());
-					tv.setText(getResources().getString(R.string.status_loading));
-					pb.setProgress(fdata.loaderProgress);
-					Log.d(TAG, "tv32 " + tv + " th " + android.os.Process.myTid());
+	public void onLoadFinished(final Loader<PicrureLoaderResult> loader, final PicrureLoaderResult data) {
+		if (!Looper.getMainLooper().equals(Looper.myLooper())) {
+			runOnUiThread(new Runnable() {
+				@Override
+				public void run() {
+					onLoadFinished(loader, data);
 				}
+			});
+		} else {
+			TextView tv = (TextView) findViewById(R.id.textViewStatusValue);
+			ProgressBar pb = (ProgressBar) findViewById(R.id.progressBarDownloading);
+
+			if (data.loaderResult == LoaderResult.LOADED) {
+				tv.setText(getResources().getString(R.string.status_loaded));
+				pb.setVisibility(View.INVISIBLE);
+				setBtnOpen();
+			} else if (data.loaderResult == LoaderResult.FAILED) {
+				tv.setText(getResources().getString(R.string.status_idle));
+				pb.setVisibility(View.INVISIBLE);
+				setBtnDownload();
+				Toast.makeText(this, getResources().getString(R.string.downloading_failed_toast), Toast.LENGTH_SHORT)
+						.show();
+			} else {
+				tv.setText(getResources().getString(R.string.status_loading));
+				findViewById(R.id.progressBarDownloading).setVisibility(View.VISIBLE);
+				pb.setProgress(data.loaderProgress);
 			}
-		});
+		}
 	}
 
 	@Override
 	public Loader<PicrureLoaderResult> onCreateLoader(int arg0, Bundle arg1) {
-		Log.d(TAG, "onCreateLoader");
 		File dir = new File(getFilesDir().getAbsolutePath());
 		dir.mkdirs();
 		File file = new File(dir, pictureName);
@@ -126,7 +108,6 @@ public class MainActivity extends FragmentActivity implements LoaderManager.Load
 
 	@Override
 	public void onLoaderReset(Loader<PicrureLoaderResult> arg0) {
-		Log.d(TAG, "onLoaderReset");
 		// TODO Auto-generated method stub
 	}
 }
--- PictureDownloader/src/ru/usb7/internship/picturedownloader/PictureLoader.java
+++ PictureDownloader/src/ru/usb7/internship/picturedownloader/PictureLoader.java
@@ -7,72 +7,68 @@ import java.io.InputStream;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.net.URLConnection;
 
 import ru.usb7.internship.picturedownloader.PicrureLoaderResult.LoaderResult;
 import android.content.Context;
 import android.support.v4.content.AsyncTaskLoader;
-import android.util.Log;
 
 public class PictureLoader extends AsyncTaskLoader<PicrureLoaderResult> {
-	private static final String TAG = "MyTag";
 	String fromUrl;
 	File toFile;
 
 	public PictureLoader(Context context, String fromUrl, File toFile) {
 		super(context);
-		Log.d(TAG, "PictureLoader");
 		this.fromUrl = fromUrl;
 		this.toFile = toFile;
 	}
 
 	@Override
 	public PicrureLoaderResult loadInBackground() {
-		Log.d(TAG, "loadInBackground");
 		try {
 			byte[] buffer = new byte[1024];
+			int fileLen = Integer.MAX_VALUE;
 			URL url = new URL(fromUrl);
-			HttpURLConnection httpUrlConnection = (HttpURLConnection) url.openConnection();
-			httpUrlConnection.connect();
+			URLConnection urlConnection = url.openConnection();
+			if (urlConnection instanceof HttpURLConnection) {
+				HttpURLConnection httpUrlConnection = (HttpURLConnection) urlConnection;
+				httpUrlConnection.connect();
 
-			String type = httpUrlConnection.getContentType();
-			Log.d(TAG, "type: " + type);
-			if (type.indexOf("image") == 0) {
-				int fileLen = httpUrlConnection.getContentLength();
-				if (fileLen < 0) {
-					fileLen = Integer.MAX_VALUE;
+				int len = httpUrlConnection.getContentLength();
+				if (len >= 0) {
+					fileLen = len;
 				}
-				InputStream in = url.openStream();
-				int len;
-				int loaded = 0;
-				FileOutputStream fOut = new FileOutputStream(toFile);
-				while ((len = in.read(buffer)) != -1) {
-					fOut.write(buffer, 0, len);
-					loaded += len;
-					final int progress = 100 * loaded / fileLen;
-
-					deliverResult(new PicrureLoaderResult(LoaderResult.LOADING, Integer.valueOf(progress)));
-					try {
-						Thread.sleep(1000);
-					} catch (InterruptedException e) {
-					}
+				String type = httpUrlConnection.getContentType();
+				if (type.indexOf("image/") != 0) {
+					return new PicrureLoaderResult(LoaderResult.FAILED, Integer.valueOf(0));
 				}
-				fOut.close();
-				in.close();
-				return new PicrureLoaderResult(LoaderResult.LOADED, Integer.valueOf(100));
 			} else {
-				return new PicrureLoaderResult(LoaderResult.FAILED, Integer.valueOf(0));
+				// lets assume that everything is ok
 			}
+
+			InputStream in = urlConnection.getInputStream();
+			FileOutputStream fOut = new FileOutputStream(toFile);
+			int len;
+			int loaded = 0;
+			deliverResult(new PicrureLoaderResult(LoaderResult.LOADING, 0));
+			while ((len = in.read(buffer)) != -1) {
+				fOut.write(buffer, 0, len);
+				loaded += len;
+				deliverResult(new PicrureLoaderResult(LoaderResult.LOADING, 100 * loaded / fileLen));
+			}
+			fOut.close();
+			in.close();
+			return new PicrureLoaderResult(LoaderResult.LOADED, 100);
 		} catch (MalformedURLException e1) {
 			e1.printStackTrace();
-			return new PicrureLoaderResult(LoaderResult.FAILED, Integer.valueOf(0));
+			return new PicrureLoaderResult(LoaderResult.FAILED, 0);
 		} catch (IOException e1) {
 			e1.printStackTrace();
-			return new PicrureLoaderResult(LoaderResult.FAILED, Integer.valueOf(0));
+			return new PicrureLoaderResult(LoaderResult.FAILED, 0);
 		}
 	}
 
 	@Override
 	protected void onStartLoading() {
-		Log.d(TAG, "onStartLoading");
 	}
 }
